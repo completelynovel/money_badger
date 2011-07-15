78b892a6919bc660a2a28e25b29fd7eee93be456
Oliver Brooks <oliver@completelynovel.com>
2011-07-15 14:59:56 +0100

    tidy test setup

--- spec/rails3/db/schema.rb
+++ spec/rails3/db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
-
+ActiveRecord::Schema.define(:version => 20110417170544) do
   create_table "money_transactions", :force => true do |t|
     t.string   "currency"
     t.integer  "amount_value"
+    t.integer  "total_value"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 end
